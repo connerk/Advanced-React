f6fe8b1b312de8c3529396afc35057038246a2c6
Kevin Conner <kevin.conner@bd.com>
2021-12-09 16:24:46 -0800

    finished module 4

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,63 @@
+import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/sick_fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data'))
+          await insertSeedData(keystone);
+      },
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        console.log(session); // {DEV}
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // graphQL query
+      User: 'id name email',
+    }),
+  })
+);
---
+++ sick-fits/backend/schemas/Product.ts
@@ -0,0 +1,37 @@
+import { integer, relationship, select, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const Product = list({
+  // TODO
+  // access:
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'draft', value: 'DRAFT' },
+        { label: 'available', value: 'AVAILABLE' },
+        { label: 'unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'draft',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+  },
+});
